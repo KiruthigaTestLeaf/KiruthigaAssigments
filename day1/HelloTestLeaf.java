94521b7b715572d02c71e8e0007c1ba830c9482a
KiruthigaTestLeaf <Kiruthigasr@gmail.com>
2025-10-03 20:18:25 +0800

    Add files via upload
    Kiruthiga Home Assignments Day 1 03102025

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to TESTLEAF Automation Course Kiruthiga First Trail");
+
+	}
+
+}
---
+++ day1/LearnOperators.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class LearnOperators {
+
+	public static void main(String[] args) {
+		
+		System.out.println(7+9);
+		System.out.println(7*9);
+		System.out.println(7-9);
+		System.out.println(7/9);
+		
+		int x=10;
+		int y=27;
+		
+	}
+
+}
